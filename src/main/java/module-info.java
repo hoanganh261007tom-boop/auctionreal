24c6f309700e1be02489f3dc15ba4f5a521dc3e9
PV <bachlmhd2007@gmail.com>
2026-05-27 14:19:05 +0700

    cập nhật module.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,20 +4,23 @@ module org.example.auctionreal {
     requires javafx.fxml;
 
     // ── JDBC + MySQL ─────────────────────────────────────────────────────────
-    requires java.sql;           // BẮT BUỘC cho java.sql.*
-        // BẮT BUỘC cho MySQL Connector/J 8.x
+    requires java.sql;
 
     // ── Mở package để JavaFX FXML có thể truy cập (reflection) ─────────────
     opens org.example.auctionreal to javafx.fxml;
     opens database to javafx.fxml;
     opens user to javafx.fxml;
+    opens model to javafx.fxml;
 
     // ── Export để các package khác dùng được ────────────────────────────────
     exports org.example.auctionreal;
     exports org.example.auctionreal.network;
+    exports org.example.auctionreal.observer;
     exports database;
     exports user;
+    exports database.model;
     exports database.dao;
     opens database.dao to javafx.fxml;
     opens org.example.auctionreal.network to javafx.fxml;
+    opens org.example.auctionreal.observer to javafx.fxml;
 }
